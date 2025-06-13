60b76a4777d389b5465f0c20867ec3dc7540d72f
kal576 <cmuuo5864@gmail.com>
2025-06-13 21:01:41 +0300

    add banking example to show oop in python

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,15 @@
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        self.account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance < amount:
+            return False
+        self.account_balance -= amount
+        return True
+
+    def display_balance(self):
+        print (f"Your current balance is {self.account_balance}")
