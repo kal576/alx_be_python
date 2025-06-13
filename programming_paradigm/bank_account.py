e54cca0e210d672891255bf51a628a7ff0a2cfcf
kal576 <cmuuo5864@gmail.com>
2025-06-13 21:09:11 +0300

    add banking example to show oop in python

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class BankAccount:
-    def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+    def __init__(self, initial_balance=0.0):
+        self.account_balance = float(initial_balance)
 
     def deposit(self, amount):
         self.account_balance += amount
