33ed84b4cc18ca986fc88e2b73f47033c47485cf
kal576 <cmuuo5864@gmail.com>
2025-06-13 21:33:55 +0300

    add file to check ZeroDivisionError

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -6,6 +6,6 @@ def safe_divider(numerator, denominator):
 
         print(f"The result of the division is {result:.1f}")
     except ZeroDivisionError:
-        return "Error: Cannot divide by zero"
+        return "Error: Cannot divide by zero."
     except ValueError:
         return "Error:Please enter numeric values only."
