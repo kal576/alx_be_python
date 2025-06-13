a0e1cb30140383da247403d5313cb040d4f5121c
kal576 <cmuuo5864@gmail.com>
2025-06-13 21:31:11 +0300

    add file to check ZeroDivisionError

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divider(numerator, denominator):
+    try:
+        num = float(numerator)
+        den = float(denominator)
+        result = num / den
+
+        print(f"The result of the division is {result:.1f}")
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero"
+    except ValueError:
+        return "Error:Please enter numeric values only"
