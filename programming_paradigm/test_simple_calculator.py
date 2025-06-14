c114e50e1915bbcdbe2afcee6b00b9ea31c9d587
kal576 <cmuuo5864@gmail.com>
2025-06-14 09:45:03 +0300

    ad test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,25 +8,29 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def test_addition(self):
         """ Testing the addition method """
-        self.assertEqual(self.calc(2, 5), 7)
-        self.assertEqual(self.calc(2, -5), -3)
-        self.assertEqual(self.calc(-2, 5), 3)
+        self.assertEqual(self.calc.add(2, 5), 7)
+        self.assertEqual(self.calc.add(2, -5), -3)
+        self.assertEqual(self.calc.add(-2, 5), 3)
 
     def test_subtraction(self):
         """ Testing the subtraction method """
-        self.assertEqual(self.calc(2, 5), -3)
-        self.assertEqual(self.calc(12, 5), 7)
+        self.assertEqual(self.calc.subtract(2, 5), -3)
+        self.assertEqual(self.calc.subtract(12, 5), 7)
 
     def test_multiplication(self):
         """ Testing the subtraction method """
-        self.assertEqual(self.calc(2, 5), 10)
-        self.assertEqual(self.calc(12, -5), -60)
-        self.assertEqual(self.calc(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(2, 5), 10)
+        self.assertEqual(self.calc.multiply(12, -5), -60)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
 
     def test_division(self):
         """ Testing the subtraction method """
-        self.assertEqual(self.calc(10, 5), 2)
-        self.assertEqual(self.calc(0, 5), "None")
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(0, 5), 0)
+        self.assertIsNone(self.calc.divide(3, 0))
+
+if __name__ == "__main__":
+    unittest.main()
 
 
 
