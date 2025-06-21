d76f3a870305b691f144171181bbd4f0fc975c0b
kal576 <cmuuo5864@gmail.com>
2025-06-21 10:01:45 +0300

    Add class_statiic_mrthods_demo.py and polymorphism_demo.py

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,29 @@
+import math 
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Must be overridden by child classes")
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+    def __str__(self):
+        return f"The area of the Rectangle is: {self.area}"
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        π = math.pi
+        return π * self.radius * self.radius
+
+    def __str__(self):
+        return f"The area of the Circle is: {self.area}"
+
+        
